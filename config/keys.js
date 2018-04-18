6b49f266e788c021b027664dbdcc0420b6f096ff
Rodrigo Sebastián Zenteno Martínez <rozenteno@Rodrigos-MacBook-Pro.local>
2018-04-18 13:24:48 -0500

    Added passport, protected routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://rozenteno:rozenteno@ds117909.mlab.com:17909/devconnector"
+  mongoURI:
+    "mongodb://rozenteno:rozenteno@ds117909.mlab.com:17909/devconnector",
+  secretOrKey: "secret"
 };
